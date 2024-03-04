8c8aab5f5275e7032a2095467ce138983eeace19
TheJerryCann <122393082+TheJerryCann@users.noreply.github.com>
2024-03-03 19:51:42 -0500

    done

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
-
-<p>Alex Rea</p>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 <ul>
-    <li><a href="status1">Status 1</li>
-    <li><a href="status2">Status 2</li>
-    <li><a href="status3">Status 3</li>
-    <li><a href="status4">Status 4</li>
-    <li><a href="status5">Status 5</li>
-    <li><a href="status6">Status 6</li>
-    <li><a href="status7">Status 7</li>
-    <li><a href="status8">Status 8</li>
-    <li><a href="status9">Status 9</li>
-    <li><a href="status10">Status 10</li>
+    <li><a href="status1.html">Status 1</li>
+    <li><a href="status2.html">Status 2</li>
+    <li><a href="status3.html">Status 3</li>
+    <li><a href="status4.html">Status 4</li>
+    <li><a href="status5.html">Status 5</li>
+    <li><a href="status7.html">Status 7</li>
+    <li><a href="status8.html">Status 8</li>
+    <li><a href="status9.html">Status 9</li>
+    <li><a href="status10.html">Status 10</li>
+    <a href="https://github.com/TheJerryCann/status-repo"><i class="fa-brands fa-github"></i></a>
 </ul>
 <?php
 $file = basename($_SERVER['PHP_SELF']);
